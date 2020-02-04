1d388ab954133fae5254db5caab3030f72ae776c
cwrlos <carloserocha27@gmail.com>
2020-02-03 22:34:11 -0300

    Exemplo simples de estrutura

---
+++ main.go
@@ -0,0 +1,12 @@
+// Essa é a estrutura de um código Go
+// Começando sempre pelo pacote
+package main
+
+// o import de bibliotecas úteis
+import "fmt"
+
+// função principal
+// igual ao C/C++
+func main() {
+	fmt.Printf("Hello World")
+}
